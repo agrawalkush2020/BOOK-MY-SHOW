010d346ec088649a8d9fb582c56a73ee77869859
kushagra agrawal <agrawalkush2020@gmail.com>
2024-11-19 23:45:47 +0530

    fetching all the movies in a asked city

--- nodejs/routes/movie.js
+++ nodejs/routes/movie.js
@@ -1,9 +1,24 @@
 import express from "express";
-import { authMiddleware } from "../middleware/middleware";
+import { authMiddleware } from "../middleware/middleware.js";
+import { Cinema, City } from "../db/movie.js";
 const router = express.Router();
 
-router.get("/get_movies_in_city", authMiddleware, (req, res) => {
+router.get("/get_movies_in_city", authMiddleware, async (req, res) => {
+    const city = req?.params?.city || "New Delhi";
+    const cityObject = await City.findOne({name:city})
+    if(!cityObject){
+        res.json({
+            movies:[]
+        })
+    }
+    console.log("cityObject",cityObject);
 
+
+    // we will find all the cinemas from the movies
+    // const cinemas = await Cinema.find({city:cityObject?._id});
+    res.json({
+        message:"success !!"
+    });
 });
 
 router.get("/get_all_shows", authMiddleware, (req, res) => {
