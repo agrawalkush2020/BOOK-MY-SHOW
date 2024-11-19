3f70a23fbd364ddd87d36ba4a0cdc90df59834d7
kushagra agrawal <agrawalkush2020@gmail.com>
2024-11-20 01:21:05 +0530

    getting all the shows for a particular movie and city

--- nodejs/routes/movie.js
+++ nodejs/routes/movie.js
@@ -4,54 +4,74 @@ import { Cinema, City, Movie, Show } from "../db/movie.js";
 const router = express.Router();
 
 router.get("/get_movies_in_city", authMiddleware, async (req, res) => {
-    const city = req?.params?.city || "New Delhi";
-    const cityObject = await City.findOne({name:city})
-    if(!cityObject){
-        res.json({
-            movies:[]
-        })
-    }
-    
-    const cinemas = await Cinema.find({city:cityObject?._id});
-    if(!cinemas){
-        res.json({
-            movies:[]
-        })
-    }
-
-    let moviesIds = [];
-    for (const cinemaObj of cinemas) {
-        const shows = await Show.find({ cinema: cinemaObj?._id });
-        {shows && (
-            shows.forEach((show)=>{
-                if(!moviesIds.includes(show?.movie)){
-                    moviesIds.push(show?.movie);
-                }
-            })
-        )}
-    }
-    if(!moviesIds.length){
-        res.json({
-            movies:[]
-        })
-    } 
-
-    let movies=[];
-    for(let id of moviesIds){
-        const movie = await Movie.findById({_id:id});
-        movies.push(movie);
-    } 
-
+  const city = req?.params?.city || "New Delhi";
+  const cityObject = await City.findOne({ name: city });
+  if (!cityObject) {
     res.json({
-        message:"success !!",
-        movies
+      movies: [],
     });
+  }
+
+  const cinemas = await Cinema.find({ city: cityObject?._id });
+  if (!cinemas) {
+    res.json({
+      movies: [],
+    });
+  }
+
+  let moviesIds = [];
+  for (const cinemaObj of cinemas) {
+    const shows = await Show.find({ cinema: cinemaObj?._id });
+    {
+      shows &&
+        shows.forEach((show) => {
+          if (!moviesIds.includes(show?.movie)) {
+            moviesIds.push(show?.movie);
+          }
+        });
+    }
+  }
+  if (!moviesIds.length) {
+    res.json({
+      movies: [],
+    });
+  }
+
+  let movies = [];
+  for (let id of moviesIds) {
+    const movie = await Movie.findById({ _id: id });
+    movies.push(movie);
+  }
+
+  res.json({
+    message: "success !!",
+    movies,
+  });
 });
 
-router.get("/get_all_shows", authMiddleware, (req, res) => {
+router.post("/get_all_shows", authMiddleware, async (req, res) => {
+  const city = req?.body?.city;
+  const movie = req?.body?.movie;
 
+  // finding the movie object
+  const movieObject = await Movie.find({ name: movie });
+  // finding the city object
+  const cityObject = await City.findOne({ name: city });
+  const cinemas = await Cinema.find({ city: cityObject?._id });
+
+  let allShows = [];
+  for (const cinemaObj of cinemas) { 
+    const shows = await Show.find({
+      cinema: cinemaObj?._id,
+      movie: movieObject[0]?._id,
+    });
+
+    shows && allShows.push(...shows);
+  }
+
+  res.json({
+    allShows,
+  });
 });
-
-
 
 export default router;
